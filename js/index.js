1c686b6c6ba9e663495b48da7836c91ab5861bd9
Randy Miller <randy918@gmail.com>
2024-10-31 14:51:25 -0500

    initial good structure todo app 10-31-24-1451

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+document.body.style.backgroundColor = 'green';
+
+//>  ████████████████████████████████████  VARIABLES
+
+const form = document.getElementById("form");
+
+//>  ████████████████████████████████████  LISTENERS
+
+form.addEventListener('click', () => {alert('thanks')});
+
+//>  ████████████████████████████████████  FUNCTIONS
+
+
+
+//>  ████████████████████████████████████  PROGRAM
+
